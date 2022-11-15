fd6cce7511b0a9e4e3607487c09e2784f5b2d23b
Ivan Golikov <gis92@rambler.ru>
2022-11-15 21:13:47 +0300

    GisJava2

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,8 @@
 public class Main {
     public static void main(String[] args) {
+        // Стоимость билета
         int priceTicket = 54841;
-
+        //Расчёт количества бонусов
         int bonusMili = priceTicket / 20;
 
         System.out.println(" Вам начислено " + bonusMili + " Миль " );
